238655175cab18672f438d390befe30f513ef76e
Ian Gunter <iangunterofamerica@yahoo.com>
2020-09-01 08:34:03 -0600

    Add taxAmount Add bad data loop

--- RunningTotal.vb
+++ RunningTotal.vb
@@ -1,12 +1,15 @@
 ﻿Option Explicit On
 Option Strict On
+Option Compare Text
 
 Module RunningTotal
 
     Sub Main()
         Dim userInput As String
-        Dim runningTotal As Integer
+        Dim runningTotal As Double
         Const TAXRATE As Double = 0.06
+        Dim badData As Boolean
+        Dim taxAmount As Double
 
         While userInput <> "Q"
             Console.WriteLine("Please select an option.")
@@ -16,22 +19,32 @@ Module RunningTotal
 
 
             If userInput = "1" Then
-                Console.WriteLine("Enter cost of item.")
-                userInput = Console.ReadLine()
 
-                Try
-                    runningTotal += CInt(userInput)
+                Do
+                    Try
+                        Console.WriteLine("Enter cost of item.")
+                        userInput = Console.ReadLine()
+                        runningTotal += CDec(userInput)
+                        badData = False
 
-                Catch ex As Exception
-                    'prompt user to give good data.
-                End Try
+                    Catch ex As Exception
+                        'prompt user to give good data.
+                        Console.WriteLine("Data must be a number.")
+                        badData = True
+                    End Try
+                Loop While badData
                 Console.WriteLine("Total: $" & runningTotal)
 
 
 
             ElseIf userInput = "2" Then
+                taxAmount = runningTotal * TAXRATE
 
-                Console.WriteLine("Tax:  $" & runningTotal * TAXRATE)
+                Console.WriteLine("Tax:  $" & taxAmount)
+
+            Else
+
+
 
             End If
 
